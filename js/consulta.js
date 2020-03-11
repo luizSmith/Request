42973c0134f933aeef0316f72aea896e0959e881
Luiz Victor <luiz.victor100@hotmail.com>
2020-03-10 23:59:52 -0300

    multiplos parametros

--- js/consulta.js
+++ js/consulta.js
@@ -1,58 +1,35 @@
-// function retorna(){
-// 	var url = document.getElementById('url').value;
-// 	//url = "api-pacientes.herokuapp.com/pacientes";
-// 	//url = "desenvolvimentoparaweb.com/javascript/json-javascript-object-notation/";
-// 	var metodo = document.querySelector('#metodo');
-// 	url = 'https://api-pacientes.herokuapp.com/pacientes';
-// 	url = 'http://www.backsite.com.br/dispositivo/service/backsite/apponepage/select.geral.bandeiras.php';
-// 	console.log(url);
-// 	console.log(metodo.value);
 
-// 	var xhr = new XMLHttpRequest();
-// 	//xhr.open(metodo.value, 'https://' + url.value);
-	
-// 	xhr.open(metodo.value,url);
-
-// 	xhr.addEventListener('load', function() {
-// 		if (this.status == 200) {
-// 			var resposta = xhr.responseText;
-// 			var paciente = JSON.stringify(resposta);
-// 			document.querySelector('#resposta').innerHTML = resposta;		
-			
-// 		} else {
-// 			console.log(this.status);
-//             console.log(this.responseText);
-//             var erroAjax = document.querySelector("#erro-ajax");
-//             erroAjax.classList.remove('invisivel');
-// 		}
-// 	});
-
-// 	xhr.send();
-// }
 
 $("form").submit(function(e){
 	e.preventDefault();
+	$.ajax({
+		url: "https://" + $(".link").val(),
+		dataType: 'json',
+		method: $(".metodo").val(),
+		success: function(result) {
+			$(".json").html(JSON.stringify(result, undefined, 2));
+  		}
+  	});
+});
 
-	//if ($(".link").val() != "") {
-		// $.post({
-  //       	//url : $(".link").val(),
-  //       	url: "https://www.backsite.com.br/publico/service/onepage/select.geral.bandeiras.php",
-  //       	type : $(".metodo").val()
-  //   	})
-    	// .done(function(msg){
-    	//      console.log('foi');
-    	// })
-	//}
+$(".mais").click(function(){
+	var div = `
+		<div class="form-row parametros">					
+    		<div class="col-md-4 mb-3">
+    			<input type="text" class="form-control key" placeholder="Key">
+    		</div>
+    		<div class="col-md-4 mb-3">
+    			<input type="text" class="form-control valor" placeholder="Valor">
+    		</div>
+    		<div class="col-md-0 mb-3">
+    			<button class="btn btn-danger menos">X</button>
+    		</div>	
+  		</div>
+	`;
+	$("#formulario").append(div);
 
+	$(".menos").click(function(){
+		$(this).parent().closest(".parametros").remove();
+	});
+});
 
-
-
-
-
- $.ajax({
-  method: "POST",
-  url: "https://www.backsite.com.br/publico/service/onepage/select.geral.bandeiras.php",
-  //data: { nome: "Pedro", email: "pedro@email.com" }
- })
-	
-})
